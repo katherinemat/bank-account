c5e82c2c86f364f1087497cd95bf54332316bc46
Kaz Matthews and Tammy Dang <tammy@>
2017-01-24 16:18:59 -0800

    combine form into one form and expand object to include deposit and withdrawl amounts.

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,39 @@
 //Business Logic
-function BankAccount(name, initial) {
+function BankAccount(name, initial, deposit, withdrawl) {
   this.name = name;
   this.initial = initial;
+  this.deposit = deposit;
+  this.withdrawl = withdrawl;
 }
 BankAccount.prototype.display = function() {
-  return this.name + ": $" + this.initial;
+  return this.name + ": $" + (this.initial + this.deposit - this.withdrawl);
 }
+
 //User Interface
 $(document).ready(function() {
   $("#bank-info").submit(function(event) {
     event.preventDefault();
+    var defaultDeposit = 0;
+    var depositAmount = 0;
+    var withdrawlAmount = 0;
+    console.log(depositAmount);
 
     var inputName = $("#name").val();
-    var defaultDeposit = parseInt($("#initial-deposit").val());
-    var customerInfo = new BankAccount(inputName, defaultDeposit);
-    $(".show-balance").append(customerInfo.display());
-    console.log(customerInfo.initial);
-  });
-
-  $("#withdrawl-deposit").submit(function(event) {
-    event.preventDefault();
-
-    var depositAmount = parseInt($("#deposit").val());
-    var withdrawlAmount = parseInt($("#withdrawl").val());
+    if (parseInt($("#initial-deposit").val()) > 0) {
+      defaultDeposit = parseInt($("#initial-deposit").val());
+    }
+    if (parseInt($("#deposit").val()) > 0) {
+      depositAmount = parseInt($("#deposit").val());
+    }
+    if (parseInt($("#withdrawl").val()) > 0) {
+      withdrawlAmount = parseInt($("#withdrawl").val());
+    }
     console.log(depositAmount);
-    console.log(withdrawlAmount);
+
+
+    var customerInfo = new BankAccount(inputName, defaultDeposit, depositAmount, withdrawlAmount);
+
+    $(".show-balance").empty().append(customerInfo.display());
   });
 
 });
