badfc3ebb7bfb899c649c76d570ec9d52bca119f
Kaz Matthews and Tammy Dang <tammy@>
2017-01-24 16:58:12 -0800

    add two submit buttons and made it work

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,50 @@
 //Business Logic
-function BankAccount(name, initial, deposit, withdrawl) {
+function BankAccount(name, initial) {
   this.name = name;
   this.initial = initial;
-  this.deposit = deposit;
-  this.withdrawl = withdrawl;
 }
 BankAccount.prototype.display = function() {
-  return this.name + ": $" + (this.initial + this.deposit - this.withdrawl);
+  return this.name + ": $" + this.initial;
 }
 
 //User Interface
 $(document).ready(function() {
+  var array = [];
+
   $("#bank-info").submit(function(event) {
     event.preventDefault();
     var defaultDeposit = 0;
-    var depositAmount = 0;
-    var withdrawlAmount = 0;
-    console.log(depositAmount);
 
     var inputName = $("#name").val();
     if (parseInt($("#initial-deposit").val()) > 0) {
       defaultDeposit = parseInt($("#initial-deposit").val());
     }
+
+    var customerInfo = new BankAccount(inputName, defaultDeposit);
+
+    array.push(customerInfo);
+
+    $(".show-balance").empty().append(customerInfo.display());
+  });
+
+  $("#withdrawl-deposit").submit(function(event) {
+    event.preventDefault();
+
+    var depositAmount = 0;
+    var withdrawlAmount = 0;
+
     if (parseInt($("#deposit").val()) > 0) {
       depositAmount = parseInt($("#deposit").val());
     }
     if (parseInt($("#withdrawl").val()) > 0) {
       withdrawlAmount = parseInt($("#withdrawl").val());
     }
-    console.log(depositAmount);
 
+    var x = function(number) {
+      return array[0].name + ": $" + number;
+    }
 
-    var customerInfo = new BankAccount(inputName, defaultDeposit, depositAmount, withdrawlAmount);
-
-    $(".show-balance").empty().append(customerInfo.display());
+    var newAmount = (array[0].initial + depositAmount - withdrawlAmount);
+    $(".show-balance").empty().append(x(newAmount));
   });
-
 });
